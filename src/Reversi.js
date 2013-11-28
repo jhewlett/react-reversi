2929519be363f2c0dd3451aa550e7129eba0e0ec
Justin Hewlett <hewlett.jus@gmail.com>
2013-11-27 23:38:14 -0700

    flip over colors in between. Detect valid moves.

--- src/Reversi.js
+++ src/Reversi.js
@@ -22,23 +22,77 @@ Game.Reversi = function() {
     _board[3][4] = Game.BLACK;
     _board[4][3] = Game.BLACK;
 
-    var toggle = function(i, j, status) {
-        if (_board[i][j] == Game.EMPTY && bordersOtherCells(i, j, status)) {
-            _board[i][j] = status;
+    var toggle = function(i, j, color) {
+        if (_board[i][j] == Game.EMPTY) {
+            var oppositeColor = color == Game.BLACK
+                ? Game.WHITE
+                : Game.BLACK;
 
-            // find everything in between
+            if (canPlayAbove(i, j, color, oppositeColor)) {
+                for (var p = i; p >= 0 && _board[p][j] !== color; p--) {
+                    _board[p][j] = color;
+                }
+            }
+            if (canPlayLeft(i, j, color, oppositeColor)) {
+                for (var p = j; p >= 0 && _board[i][p] !== color; p--) {
+                    _board[i][p] = color;
+                }
+            }
+            if (canPlayRight(i, j, color, oppositeColor)) {
+                for (var p = j; p < 8 && _board[i][p] !== color; p++) {
+                    _board[i][p] = color;
+                }
+            }
+            if (canPlayBelow(i, j, color, oppositeColor)) {
+                for (var p = i; p < 8 && _board[p][j] !== color; p++) {
+                    _board[p][j] = color;
+                }
+            }
         }
     };
 
-    var bordersOtherCells = function(i, j, newStatus) {
+    var canPlayAbove = function(i, j, color, oppositeColor) {
         var above = i == 0 ? Game.EMPTY : _board[i - 1][j];
-        var left = j == 0 ? Game.EMPTY : _board[i][j - 1];
-        var right = j == 7 ? Game.EMPTY : _board[i][j + 1];
+
+        return above === oppositeColor && columnContainsColor(i, 0, i - 1, color);
+    };
+
+    var canPlayBelow = function(i, j, color, oppositeColor) {
         var below = i == 7 ? Game.EMPTY : _board[i + 1][j];
 
-        var oppositeStatus = newStatus == Game.BLACK ? Game.WHITE : Game.BLACK;
+        return below === oppositeColor && columnContainsColor(j, i + 1, 7, color);
+    };
 
-        return above === oppositeStatus || left === oppositeStatus || right === oppositeStatus || below === oppositeStatus;
+    var canPlayLeft = function(i, j, color, oppositeColor) {
+        var left = j == 0 ? Game.EMPTY : _board[i][j - 1];
+
+        return left === oppositeColor && rowContainsColor(i, 0, j - 1, color);
+    };
+
+    var canPlayRight = function(i ,j, color, oppositeColor) {
+        var right = j == 7 ? Game.EMPTY : _board[i][j + 1];
+
+        return right === oppositeColor && rowContainsColor(i, j + 1, 7, color);
+    }
+
+    var columnContainsColor = function(j, a, b, color) {
+        for (var p = a; p <= b; p++) {
+            if (_board[p][j] === color) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    var rowContainsColor = function(i, a, b, color) {
+        for (var p = a; p <= b; p++) {
+            if (_board[i][p] === color) {
+                return true;
+            }
+        }
+
+        return false;
     };
 
     var getStatus = function(i, j) {
--- test/ReversiTests.js
+++ test/ReversiTests.js
@@ -6,7 +6,7 @@ test("toggle switches the selected and anything all inbetween", function() {
     reversi.toggle(2, 3, Game.BLACK);
 
     assertEquals(Game.BLACK, reversi.getStatus(2, 3));
-    //assertEquals(Game.BLACK, reversi.getStatus(2, 3));
+    assertEquals(Game.BLACK, reversi.getStatus(3, 3));
 });
 
 test("toggle test 2", function() {
@@ -26,6 +26,42 @@ test("toggling a cell that does not border another of the opposite color does no
     assertEquals(Game.EMPTY, reversi.getStatus(2, 3));
 });
 
+test("toggling a cell that does not surround a row does nothing - below", function() {
+    var reversi = new Game.Reversi();
+
+    reversi.toggle(3, 2, Game.BLACK);
+    reversi.toggle(2, 2, Game.WHITE);
+
+    assertEquals(Game.EMPTY, reversi.getStatus(2, 2));
+});
+
+test("toggling a cell that does not surround a row does nothing - above", function() {
+    var reversi = new Game.Reversi();
+
+    reversi.toggle(4, 2, Game.WHITE);
+    reversi.toggle(5, 2, Game.BLACK);
+
+    assertEquals(Game.EMPTY, reversi.getStatus(5, 2));
+});
+
+test("toggling a cell that does not surround a row does nothing - left", function() {
+    var reversi = new Game.Reversi();
+
+    reversi.toggle(2, 4, Game.WHITE);
+    reversi.toggle(2, 5, Game.BLACK);
+
+    assertEquals(Game.EMPTY, reversi.getStatus(2, 5));
+});
+
+test("toggling a cell that does not surround a row does nothing - right", function() {
+    var reversi = new Game.Reversi();
+
+    reversi.toggle(2, 3, Game.BLACK);
+    reversi.toggle(3, 2, Game.WHITE);
+
+    assertEquals(Game.EMPTY, reversi.getStatus(3, 2));
+});
+
 test("Cannot toggle an occupied cell", function() {
     var reversi = new Game.Reversi();
 
@@ -35,7 +71,7 @@ test("Cannot toggle an occupied cell", function() {
     assertEquals(Game.BLACK, reversi.getStatus(2, 3));
 });
 
-test("four squares are set at beginning of game", function() {
+test("four squares are set at beginning of game, the rest are empty", function() {
     var reversi = new Game.Reversi();
 
     assertEquals(Game.WHITE, reversi.getStatus(3, 3));
@@ -52,3 +88,5 @@ test("four squares are set at beginning of game", function() {
         }
     }
 });
+
+//diaganol moves
