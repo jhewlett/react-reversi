f4705b8792881af58f2e597afdc6aca5ace992d9
Justin Hewlett <hewlett.jus@gmail.com>
2013-11-29 14:45:17 -0700

    removed lots of duplication

--- test/ReversiTests.js
+++ test/ReversiTests.js
@@ -1,92 +1,108 @@
-module("Reversi tests");
+module("Board tests");
 
-test("toggle switches the selected and anything all inbetween", function() {
-    var reversi = new Game.Reversi();
+test("toggle switches the selected and anything inbetween", function() {
+    var board = new Reversi.Board();
 
-    reversi.toggle(2, 3, Game.BLACK);
+    board.toggle(2, 3, Reversi.BLACK);
 
-    assertEquals(Game.BLACK, reversi.getStatus(2, 3));
-    assertEquals(Game.BLACK, reversi.getStatus(3, 3));
-});
-
-test("toggle test 2", function() {
-    var reversi = new Game.Reversi();
-
-    reversi.toggle(4, 2, Game.WHITE);
-
-    assertEquals(Game.WHITE, reversi.getStatus(4, 2));
-    //assertEquals(Game.WHITE, reversi.getStatus(4, 3));
+    assertEquals(Reversi.BLACK, board.getStatus(2, 3));
+    assertEquals(Reversi.BLACK, board.getStatus(3, 3));
 });
 
 test("toggling a cell that does not border another of the opposite color does nothing", function() {
-    var reversi = new Game.Reversi();
+    var board = new Reversi.Board();
 
-    reversi.toggle(2, 3, Game.WHITE);
+    board.toggle(2, 3, Reversi.WHITE);
 
-    assertEquals(Game.EMPTY, reversi.getStatus(2, 3));
+    assertEquals(Reversi.EMPTY, board.getStatus(2, 3));
 });
 
 test("toggling a cell that does not surround a row does nothing - below", function() {
-    var reversi = new Game.Reversi();
+    var board = new Reversi.Board();
 
-    reversi.toggle(3, 2, Game.BLACK);
-    reversi.toggle(2, 2, Game.WHITE);
+    board.toggle(3, 2, Reversi.BLACK);
+    board.toggle(2, 2, Reversi.WHITE);
 
-    assertEquals(Game.EMPTY, reversi.getStatus(2, 2));
+    assertEquals(Reversi.EMPTY, board.getStatus(2, 2));
 });
 
 test("toggling a cell that does not surround a row does nothing - above", function() {
-    var reversi = new Game.Reversi();
+    var board = new Reversi.Board();
 
-    reversi.toggle(4, 2, Game.WHITE);
-    reversi.toggle(5, 2, Game.BLACK);
+    board.toggle(4, 2, Reversi.WHITE);
+    board.toggle(5, 2, Reversi.BLACK);
 
-    assertEquals(Game.EMPTY, reversi.getStatus(5, 2));
+    assertEquals(Reversi.EMPTY, board.getStatus(5, 2));
 });
 
 test("toggling a cell that does not surround a row does nothing - left", function() {
-    var reversi = new Game.Reversi();
+    var board = new Reversi.Board();
 
-    reversi.toggle(2, 4, Game.WHITE);
-    reversi.toggle(2, 5, Game.BLACK);
+    board.toggle(2, 4, Reversi.WHITE);
+    board.toggle(2, 5, Reversi.BLACK);
 
-    assertEquals(Game.EMPTY, reversi.getStatus(2, 5));
+    assertEquals(Reversi.EMPTY, board.getStatus(2, 5));
 });
 
 test("toggling a cell that does not surround a row does nothing - right", function() {
-    var reversi = new Game.Reversi();
+    var board = new Reversi.Board();
 
-    reversi.toggle(2, 3, Game.BLACK);
-    reversi.toggle(3, 2, Game.WHITE);
+    board.toggle(2, 3, Reversi.BLACK);
+    board.toggle(3, 2, Reversi.WHITE);
 
-    assertEquals(Game.EMPTY, reversi.getStatus(3, 2));
+    assertEquals(Reversi.EMPTY, board.getStatus(3, 2));
+});
+
+test("can capture in all directions in one turn", function() {
+    var board = new Reversi.Board();
+
+    board.set(0, 2, Reversi.BLACK);
+    board.set(1, 2, Reversi.WHITE);
+    board.set(2, 0, Reversi.BLACK);
+    board.set(2, 1, Reversi.WHITE);
+    board.set(2, 3, Reversi.WHITE);
+    board.set(2, 4, Reversi.BLACK);
+    board.set(3, 2, Reversi.WHITE);
+    board.set(4, 2, Reversi.BLACK);
+
+    board.toggle(2, 2, Reversi.BLACK);
+
+    assertEquals(Reversi.BLACK, board.getStatus(1, 2));
+    assertEquals(Reversi.BLACK, board.getStatus(2, 1));
+    assertEquals(Reversi.BLACK, board.getStatus(2, 3));
+    assertEquals(Reversi.BLACK, board.getStatus(3, 2));
+
+    assertEquals(Reversi.BLACK, board.getStatus(2, 2));
 });
 
 test("Cannot toggle an occupied cell", function() {
-    var reversi = new Game.Reversi();
+    var board = new Reversi.Board();
 
-    reversi.toggle(2, 3, Game.BLACK);
-    reversi.toggle(2, 3, Game.WHITE);
+    board.toggle(2, 3, Reversi.BLACK);
+    board.toggle(2, 3, Reversi.WHITE);
 
-    assertEquals(Game.BLACK, reversi.getStatus(2, 3));
+    assertEquals(Reversi.BLACK, board.getStatus(2, 3));
 });
 
 test("four squares are set at beginning of game, the rest are empty", function() {
-    var reversi = new Game.Reversi();
+    var board = new Reversi.Board();
 
-    assertEquals(Game.WHITE, reversi.getStatus(3, 3));
-    assertEquals(Game.WHITE, reversi.getStatus(4, 4));
+    assertEquals(Reversi.WHITE, board.getStatus(3, 3));
+    assertEquals(Reversi.WHITE, board.getStatus(4, 4));
 
-    assertEquals(Game.BLACK, reversi.getStatus(3, 4));
-    assertEquals(Game.BLACK, reversi.getStatus(4, 3));
+    assertEquals(Reversi.BLACK, board.getStatus(3, 4));
+    assertEquals(Reversi.BLACK, board.getStatus(4, 3));
 
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
             if ((i < 3 || i > 4) && (j < 3 || j > 4)) {
-                assertEquals(Game.EMPTY, reversi.getStatus(i, j))
+                assertEquals(Reversi.EMPTY, board.getStatus(i, j))
             }
         }
     }
 });
 
 //diaganol moves
+//have a move that switches players
+//detect when no moves available (or have option to pass)
+//detect when game over
