31dfaf929639d2c5fd198240722bbf03814a794c
Justin Hewlett <hewlett.jus@gmail.com>
2015-03-16 01:08:58 -0600

    move some methods off of the store and into private functions.

--- js/stores/GameStore.js
+++ js/stores/GameStore.js
@@ -1,16 +1,57 @@
 var Reflux = require('reflux');
 var Player = require('../lib/Player');
-
 var Board = require('../lib/Board');
-
 var GameActions = require('../actions/GameActions');
-
 var merge = require('object-assign');
+
+var getScoreForPlayer = function(player, board) {
+  var score = 0;
+
+  for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 8; j++) {
+          if (board.getStatus(i, j) === player) {
+              score += 1;
+          }
+      }
+  }
+
+  return score;
+}
+
+var checkEndOfGame = function(player1Score, player2Score, cb) {
+  if (player1Score === 0) {
+      cb({ winnerMessage: "Player 2 wins!" });
+  } else if (player2Score === 0) {
+      cb({ winnerMessage: "Player 1 wins!" });
+  } else if (player1Score + player2Score === 64) {
+      if (player1Score === player2Score) {
+          cb({ winnerMessage: "Tie!" });
+      } else if (player1Score > player2Score) {
+          cb({ winnerMessage: "Player 1 wins!" });
+      } else {
+          cb({ winnerMessage: "Player 2 wins!" });
+      }
+  } else {
+      return false;
+  }
+
+  return true;
+}
+
+var newGame = function() {
+  return {
+    currentPlayer: Player.One,
+    player1Score: 2,
+    player2Score: 2,
+    board: new Board(),
+    winnerMessage: ''
+  };
+};
 
 module.exports = Reflux.createStore({
     listenables: [GameActions],
     init() {
-      this.state = this.newGame();
+      this.state = newGame();
     },
     getInitialState() {
         return this.state;
@@ -25,9 +66,12 @@ module.exports = Reflux.createStore({
       var result = this.state.board.makeMove(row, col, this.state.currentPlayer);
 
       if (result) {
-          this.updateScores();
+          this.update({
+            player1Score: getScoreForPlayer(Player.One, this.state.board),
+            player2Score: getScoreForPlayer(Player.Two, this.state.board)
+          });
 
-          var gameEnded = this.checkEndOfGame();
+          var gameEnded = checkEndOfGame(this.state.player1Score, this.state.player2Score, (newState) => this.update(newState));
 
           if (!gameEnded) {
               GameActions.switchPlayer();
@@ -35,54 +79,7 @@ module.exports = Reflux.createStore({
       }
     },
     onReset() {
-      this.update(this.newGame());
-    },
-    newGame() {
-      return {
-          currentPlayer: Player.One,
-          player1Score: 2,
-          player2Score: 2,
-          board: new Board(),
-          winnerMessage: ''
-      };
-    },
-    updateScores() {
-        this.update({
-          player1Score: this.getScoreForPlayer(Player.One),
-          player2Score: this.getScoreForPlayer(Player.Two)
-        });
-    },
-    getScoreForPlayer(player) {
-      var score = 0;
-
-      for (var i = 0; i < 8; i++) {
-          for (var j = 0; j < 8; j++) {
-              if (this.state.board.getStatus(i, j) === player) {
-                  score += 1;
-              }
-          }
-      }
-
-      return score;
-    },
-    checkEndOfGame() {
-      if (this.state.player1Score === 0) {
-          this.update({ winnerMessage: "Player 2 wins!" });
-      } else if (this.state.player2Score === 0) {
-          this.update({ winnerMessage: "Player 1 wins!" });
-      } else if (this.state.player1Score + this.state.player2Score === 64) {
-          if (this.state.player1Score === this.state.player2Score) {
-              this.update({ winnerMessage: "Tie!" });
-          } else if (this.state.player1Score > this.state.player2Score) {
-              this.update({ winnerMessage: "Player 1 wins!" });
-          } else {
-              this.update({ winnerMessage: "Player 2 wins!" });
-          }
-      } else {
-          return false;
-      }
-
-      return true;
+      this.update(newGame());
     },
     update(newState) {
       this.state = merge(this.state, newState);
