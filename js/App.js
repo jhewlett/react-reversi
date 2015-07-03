42d84ff26224748afcddc744008c6d4043a4dfca
Justin Hewlett <hewlett.jus@gmail.com>
2015-07-03 11:41:10 -0600

    Remove unused provider

--- js/App.js
+++ js/App.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import { createRedux } from 'redux';
-import { Provider } from 'redux/react';
-import game from './stores/game';
+
 import Game from './components/Game';
 
-const redux = createRedux({ game });
-
-React.render(
-   (<Provider redux={redux}>
-      {() =>
-         <Game />
-      }
-   </Provider>), document.getElementById('react-app'));
+React.render(<Game />, document.getElementById('react-app'));
