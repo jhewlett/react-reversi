13d50817bd2d77ad0a97aa7cfe9af3ab1425add4
Justin Hewlett <hewlett.jus@gmail.com>
2015-07-13 17:21:56 -0600

    polish connectToStore

--- js/components/Game.js
+++ js/components/Game.js
@@ -6,28 +6,28 @@ import ButtonGroup from './ButtonGroup';
 import Player from '../lib/Player';
 import { getScore } from '../lib/Board';
 import { Stack, Map, List } from 'immutable';
-import createConnector from '../stores/connectToStore';
+import { default as createConnector, toStores } from '../stores/connectToStore';
 import redux from '../redux';
 
-const { connect, connectToStore } = createConnector(redux)
+const connect = createConnector(redux)
 
-@connectToStore('game')
+@connect(state => state.game)
 export default class Game {
-   // static propTypes = {
-   //    boardHistory: React.PropTypes.instanceOf(Stack).isRequired,
-   //    playerHint: React.PropTypes.instanceOf(Map).isRequired,
-   //    board: React.PropTypes.instanceOf(List).isRequired,
-   //    currentPlayer: React.PropTypes.number.isRequired
-   // }
+   static propTypes = {
+      boardHistory: React.PropTypes.instanceOf(Stack).isRequired,
+      playerHint: React.PropTypes.instanceOf(Map).isRequired,
+      board: React.PropTypes.instanceOf(List).isRequired,
+      currentPlayer: React.PropTypes.number.isRequired
+   }
    render() {
-      const score = getScore(this.props.game.board);
+      const score = getScore(this.props.board);
 
       return (
          <div>
-            <PlayerInfo currentPlayer={this.props.game.currentPlayer} score={score} />
+            <PlayerInfo currentPlayer={this.props.currentPlayer} score={score} />
             <WinnerMessage score={score} />
-            <Board board={this.props.game.board} playerHint={this.props.game.playerHint} />
-            <ButtonGroup score={score} boardHistory={this.props.game.boardHistory} />
+            <Board board={this.props.board} playerHint={this.props.playerHint} />
+            <ButtonGroup score={score} boardHistory={this.props.boardHistory} />
          </div>
       );
    }
--- js/stores/connectToStore.js
+++ js/stores/connectToStore.js
@@ -1,47 +1,33 @@
 import React from 'react'
 import { Connector, Provider } from 'redux/react'
 
-function createConnectorWrapper(redux, select, DecoratedComponent) {
-   return class ConnectorWrapper extends React.Component {
-     render() {
-       return (
-          <Provider redux={redux}>
-            {() => (
-                <Connector select={select}>
-                  {(props) => <DecoratedComponent {...props} />}
-                </Connector>
-             )
-            }
-          </Provider>
-       )
-     }
+export function toStores(...storeNames) {
+  return state => {
+     return storeNames.reduce((allStoresState, storeName) => {
+       return {
+         ...allStoresState,
+         [storeName]: state[storeName]
+       }
+     }, {})
    }
 }
 
 export default function createConnector(redux) {
-  function connectToStore(...storeNames) {
-      function select(state) {
-         return storeNames.reduce((allStoresState, storeName) => {
-             return {
-                ...allStoresState,
-               [storeName]: state[storeName]
-            }
-         }, {})
-      }
-
+  return function connect(select) {
     return function decorateSource(DecoratedComponent) {
-        return createConnectorWrapper(redux, select, DecoratedComponent)
+      return class ConnectorWrapper extends React.Component {
+        render() {
+          return (
+            <Provider redux={redux}>
+              {() => (
+                <Connector select={select}>
+                  {(props) => <DecoratedComponent {...props} />}
+                </Connector>)
+              }
+            </Provider>
+          )
+        }
       }
-    }
-
-  function connect(customSelect) {
-    return function decorateSource(DecoratedComponent) {
-      return createConnectorWrapper(redux, customSelect, DecoratedComponent)
     }
   }
-
-  return {
-     connect,
-     connectToStore
- }
 }
